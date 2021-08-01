19dcd430635b9f6ea6797457441a7c45dffec9c0
SreshthTiwari <sreshthtiwari13@gmail.com>
2021-08-01 20:56:20 +0530

    fixed navbar issue

--- src/script.js
+++ src/script.js
@@ -1,30 +1,30 @@
 let sidebar = document.querySelector(".sidebar");
 let closeBtn = document.querySelector("#btn");
 let searchBtn = document.querySelector(".bx-search");
-// let links = document.getElementsByClassName("link");
-// let list_items = document.getElementsByTagName("li");
-// const links_array = Array.from(links);
-// const array_list_items = Array.from(list_items);
+let links = document.getElementsByClassName("link");
+let list_items = document.getElementsByTagName("li");
+const links_array = Array.from(links);
+const array_list_items = Array.from(list_items);
 
-// window.onload = remove_links;
+window.onload = remove_links;
 
-// function remove_links(){
-//   links_array.forEach(element => {
-//     element.remove();
-//   });
-//   // console.log("removed links");
-// }
+function remove_links(){
+  links_array.forEach(element => {
+    element.remove();
+  });
+  // console.log("removed links");
+}
 
-// function addlinks(){
-//   var counter = 0;
+function addlinks(){
+  var counter = 0;
   
-//   array_list_items.forEach(element =>{
-//     element.appendChild(links_array[counter]);
-//     counter += 1;
-//   });
+  array_list_items.forEach(element =>{
+    element.appendChild(links_array[counter]);
+    counter += 1;
+  });
 
-//   // console.log("added links");
-// }
+  // console.log("added links");
+}
 
 
 closeBtn.addEventListener("click", ()=>{
